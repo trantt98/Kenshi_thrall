f2375cf1a9d58f50ef4726335e015a2288092198
Kim Tin Tran <fedora.laptop@tran-kt.com>
2026-04-17 20:38:59 +0200

    if keyword is right dont put all the other keywords in false.txt

--- test.py
+++ test.py
@@ -28,37 +28,39 @@ def file_contains(filename:str,word:str):
 def questions(x:int):
     to_sort = []
     print("give in the questions")
+    
     for i in range(x):
-       to_check = input()
-       to_sort.append(to_check)
-
-    for i in range(x):
+        to_check = input()
+        to_sort.append(to_check)
 
         if file_contains(correct, to_sort[i]):
             print_green("this word is true")
-            print(" "+ to_sort[i])
-
-        elif file_contains(wrong,to_sort[i]):
-            print_red("this word is wrong")
             print(" " + to_sort[i])
 
-       
+        elif file_contains(wrong, to_sort[i]):
+            print_red("this word is wrong")
+            print(" " + to_sort[i])
+   
+
+
+    for i in range(x):
+
+              
+        print("did the question fail? n/number")
+        answer = input()
+        if(answer == "n"):
+            print("which one is correct?")
+            number = input()
+            index = int(number) -1
+            put_correct_word(to_sort[index])
+
+            for i in range(len(to_sort)):
+                if file_contains(wrong,to_sort[i]) == False:
+                    put_false_word(to_sort[i])
+
         else: 
-            print("did the question fail? n/number")
-            answer = input()
-            if(answer == "n"):
-                print("which one is correct?")
-                number = input()
-                index = int(number) -1
-                put_correct_word(to_sort[index])
-
-                for i in range(len(to_sort)):
-                    if file_contains(wrong,to_sort[i]) == False:
-                        put_false_word(to_sort[i])
-
-            else: 
-                put_false_word(to_sort[int(answer)-1])
-                break
+            put_false_word(to_sort[int(answer)-1])
+            break
 
 
 for i in range(1000):
